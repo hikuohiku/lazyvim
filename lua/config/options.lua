47bb17686a6159dfe51956baa0b8ea1892c23494
hikuohiku <hikuohiku@gmail.com>
2024-09-20 19:09:32 +0900

    help ja

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- help ja
+vim.o.helplang = 'ja,en'
+
 -- vim.g.ayucolor = "light"
 -- vim.go.background = "light"
 vim.o.swapfile = false

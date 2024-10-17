50e2b9c03d25b47818debe6cb287f9379ababc2e
Hiroki Ando <hikuohiku@gmail.com>
2024-10-18 00:57:41 +0900

    copilot

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -11,5 +11,14 @@ return {
       })
     end
   },
-  { "sophacles/vim-processing" }
+  { "sophacles/vim-processing" },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.completion.completeopt = "menu,menuone,noinsert,noselect"
+      opts.preselect = cmp.PreselectMode.None
+      opts.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
+    end,
+  },
 }

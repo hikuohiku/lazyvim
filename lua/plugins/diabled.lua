0ce8a2276a334496b847a11d3a15ab1647cb000b
hikuohiku <hikuohiku@gmail.com>
2024-10-18 01:15:19 +0900

    available mason

--- lua/plugins/diabled.lua
+++ lua/plugins/diabled.lua
@@ -1,4 +1,16 @@
+local function getHostname()
+  local f = io.popen("hostname")
+  if f == nil then
+    return ""
+  end
+  local hostname = f:read("*a") or ""
+  f:close()
+  hostname = string.gsub(hostname, "\n$", "")
+  return hostname
+end
+
+local hostname = getHostname()
 return {
   -- diable mason due to nixos incompatibility
-  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason.nvim", enabled = hostname ~= 'hikuo-desktop' },
 }
